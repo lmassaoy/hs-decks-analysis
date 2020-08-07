9aeb33693af8ce273c402b9b033ef9f37685f36b
Luis Yamada <Yamada@MacBook-Pro.local>
2020-08-07 13:08:32 -0300

    (develop): udpates over card exploration app & image_downloader

---
+++ python/cards_v2.py
@@ -0,0 +1,119 @@
+import pandas as pd
+import streamlit as st
+from PIL import Image
+import json
+
+
+all_cards = "/Users/Yamada/Git/git-projects/hs-decks-analysis/data/datasets/cards/cards.json"
+collectible_cards = "/Users/Yamada/Git/git-projects/hs-decks-analysis/data/datasets/cards/cards.collectible.json"
+hs_logo = "/Users/Yamada/Git/git-projects/hs-decks-analysis/data/images/logos/hearthstone_title.png"
+cards_photo = "/Users/Yamada/Git/git-projects/hs-decks-analysis/data/images/design/hearthstone_cards_sample.png"
+
+hs_logo_image = Image.open(hs_logo)
+cards_image = Image.open(cards_photo)
+
+
+st.image(hs_logo_image, use_column_width=True)
+st.title("Cards - Exploration Module")
+st.image(cards_image, use_column_width=True)
+
+@st.cache
+def generate_json_df(file_path):
+    return pd.read_json(file_path)
+
+cards_df = generate_json_df(collectible_cards)
+
+with open(collectible_cards) as f:
+  dict_cards = json.load(f)
+
+
+# --------------- Card Selection (Sidebar) ---------------
+
+st.sidebar.header("**Card Selection**")
+
+@st.cache
+def get_list_cards(dataframe):
+    return dataframe["name"].drop_duplicates().values.tolist()
+
+list_cards = sorted(get_list_cards(cards_df))
+specific_card_selectbox = st.sidebar.selectbox('Select a card',list_cards)
+
+specific_card_index = cards_df.index[cards_df["name"] == specific_card_selectbox].tolist()
+
+card_id = cards_df["id"][cards_df["name"] == specific_card_selectbox].tolist()[0] + '.png'
+image_render_path = "/Users/Yamada/Git/git-projects/hs-decks-analysis/data/images/cards/renders/256x/"
+
+card_image = Image.open(image_render_path+card_id)
+
+st.sidebar.image(card_image)
+
+st.sidebar.subheader("**Card - Json Object**")
+
+st.sidebar.json(dict_cards[specific_card_index[0]])
+
+# --------------- Dataframe ---------------
+
+st.header("**Cards Dataframe**")
+st.text("Use the filters below to explore the dataframe")
+
+st.header("**Filters**")
+
+specific_card_text = st.text_input("Card Name (you can only type part of the name)")
+
+
+@st.cache()
+def get_list_card_class(dataframe):
+    return dataframe["cardClass"].drop_duplicates().values.tolist()
+
+list_class = get_list_card_class(cards_df)
+class_multiselect = st.multiselect('Classes',list_class,list_class)
+
+
+@st.cache
+def get_list_rarity(dataframe):
+    return dataframe["rarity"].drop_duplicates().values.tolist()
+
+list_rarity = get_list_rarity(cards_df)
+rarity_multiselect = st.multiselect('Rarity',list_rarity,list_rarity)
+
+
+@st.cache
+def get_list_set(dataframe):
+    return dataframe["set"].drop_duplicates().values.tolist()
+
+list_set = get_list_set(cards_df)
+set_multiselect = st.multiselect('Set',list_set,list_set)
+
+
+@st.cache
+def get_list_type(dataframe):
+    return dataframe["type"].drop_duplicates().values.tolist()
+
+type_rarity = get_list_type(cards_df)
+type_multiselect = st.multiselect('Type',type_rarity,type_rarity)
+
+
+@st.cache()
+def get_min_max_costs(dataframe):
+    min_card_cost = dataframe["cost"].min()
+    max_card_cost = dataframe["cost"].max()
+    return {"min": min_card_cost, "max": max_card_cost}
+
+dict_min_max_cost = get_min_max_costs(cards_df)
+min_card_cost = int(dict_min_max_cost["min"])
+max_card_cost = int(dict_min_max_cost["max"])
+
+cost_slider = st.slider('Card Cost',min_card_cost,max_card_cost,[min_card_cost,max_card_cost],1)
+
+
+if specific_card_text != '':
+    sample_df = cards_df[cards_df['name'].str.contains(specific_card_text)]
+else:
+    sample_df = cards_df[cards_df['cardClass'].isin(class_multiselect)
+                    & cards_df['rarity'].isin(rarity_multiselect)
+                    & cards_df['type'].isin(type_multiselect)
+                    & cards_df['cost'].between(cost_slider[0], cost_slider[1])]
+
+st.dataframe(sample_df.head(10000))
+
+st.write(f"Full DF Size: {len(cards_df.index)} :speak_no_evil:")
---
+++ python/utils/images_downloader.md
@@ -0,0 +1,25 @@
+# images_downloader.py
+
+## How am I
+I am a python job capable to download all render and tile images from [https://hearthstonejson.com/](https://hearthstonejson.com/).
+
+## How do I do my job
+I run three threads to have a better performance to download everything I need, by using the python lib concurrent.futures. If you're interested, please read my script :)
+
+## How run me
+First: make sure you already have the directories where the images will be downloaded
+- /hs-decks-analysis/data/images/cards/renders/256x/
+- /hs-decks-analysis/data/images/cards/renders/512x/
+- /hs-decks-analysis/data/images/cards/tiles/
+
+Now you just need to run me:
+```
+$ python3 python/utils/images_downloader.py
+
+Size of the card list: 2674
+Starting: Download Render 512x
+Starting: Download Render 256
+Starting: Download Tiles
+--- It took 3907.6407327651978 seconds to complete all downloads ---
+```
+Yeah, it may take a while to download everything (around 2 GBs)
